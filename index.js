dcdb7998c769ad4c03a6c4d1a463c9361f39f561
Chanda Hopkins <chandahopkins@outlook.com>
2023-05-30 23:59:41 -0500

    completed

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const returnLastTwoDrivers = function (drivers){return (drivers.slice(2, 4))}
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
-createFareMultiplier(num1)
+const createFareMultiplier = function (num1){return (fare) => num1 * num1}
+
+const fareDoubler = function (createFareMultiplier){return createFareMultiplier * 2}
+
+const fareTripler = function (createFareMultiplier){return createFareMultiplier * 3}
+
+function selectDifferentDrivers(drivers, selectingDrivers){return selectingDrivers(drivers)}
 
 
 
